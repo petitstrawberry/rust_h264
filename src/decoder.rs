20f368cd10edaa11075a45b1b54b76941953617c
Victor Loh <fozayr@gmail.com>
2026-04-01 17:43:29 -0700

    Refactor decoder.rs (VIII)

--- src/decoder.rs
+++ src/decoder.rs
@@ -4385,9 +4385,9 @@ impl Decoder {
                     );
 
                     // Chroma (same as I4x4 CABAC path)
-                    let chroma_width = (width / 2) as usize;
-                    let chroma_mb_x = mb_x / 2;
-                    let chroma_mb_y = mb_y / 2;
+                    let _chroma_width = (width / 2) as usize;
+                    let _chroma_mb_x = mb_x / 2;
+                    let _chroma_mb_y = mb_y / 2;
                     let (pred_u, pred_v) = make_ctx!().predict_chroma_intra(
                         mb_x,
                         mb_y,
@@ -4507,57 +4507,30 @@ impl Decoder {
                         }
                     }
 
-                    for (plane_dc, plane_ac, pred_plane, frame_plane, scale_idx) in [
-                        (
+                    {
+                        let mut ctx = make_ctx!();
+                        ctx.reconstruct_chroma_plane(
                             &mut chroma_dc_cb,
                             &chroma_ac_cb,
                             &pred_u,
-                            &mut frame.u,
-                            1usize,
-                        ),
-                        (
+                            true,
+                            cbp_chroma,
+                            qp_c,
+                            &pps.scaling_list_4x4[1],
+                            mb_x,
+                            mb_y,
+                        );
+                        ctx.reconstruct_chroma_plane(
                             &mut chroma_dc_cr,
                             &chroma_ac_cr,
                             &pred_v,
-                            &mut frame.v,
-                            2usize,
-                        ),
-                    ] {
-                        let chroma_scale = &pps.scaling_list_4x4[scale_idx];
-                        if cbp_chroma >= 1 {
-                            inverse_hadamard_2x2(plane_dc);
-                            dequant_chroma_dc(plane_dc, qp_c, chroma_scale[0]);
-                        }
-                        let mut chroma_residual = [0i32; 64];
-                        for blk in 0..4 {
-                            let blk_row = (blk / 2) * 4;
-                            let blk_col = (blk % 2) * 4;
-                            let mut block_raster = [0i32; 16];
-                            block_raster[0] = plane_dc[blk];
-                            if cbp_chroma >= 2 {
-                                for scan_idx in 0..15 {
-                                    let (r, c) = ZIGZAG_4X4[scan_idx + 1];
-                                    block_raster[r * 4 + c] = plane_ac[blk][scan_idx];
-                                }
-                                dequant_4x4_ac_raster(&mut block_raster, qp_c, chroma_scale);
-                            }
-                            inverse_dct_4x4(&mut block_raster);
-                            for r in 0..4 {
-                                for c in 0..4 {
-                                    chroma_residual[(blk_row + r) * 8 + blk_col + c] =
-                                        block_raster[r * 4 + c];
-                                }
-                            }
-                        }
-                        for y in 0..8 {
-                            for x in 0..8 {
-                                let val = (pred_plane[y * 8 + x] as i32
-                                    + chroma_residual[y * 8 + x])
-                                    .clamp(0, 255) as u8;
-                                frame_plane[(chroma_mb_y + y) * chroma_width + chroma_mb_x + x] =
-                                    val;
-                            }
-                        }
+                            false,
+                            cbp_chroma,
+                            qp_c,
+                            &pps.scaling_list_4x4[2],
+                            mb_x,
+                            mb_y,
+                        );
                     }
 
                     mb_info[mb_idx] = MbInfo {
